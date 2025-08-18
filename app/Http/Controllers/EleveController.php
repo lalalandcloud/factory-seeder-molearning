406193296189624a1f02e9a838a1e693915a5389
molengeek <saoussan.elk@gmail.com>
2025-08-18 15:31:07 +0200

    fc

---
+++ app/Http/Controllers/EleveController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Eleve;
+use App\Http\Requests\StoreEleveRequest;
+use App\Http\Requests\UpdateEleveRequest;
+
+class EleveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEleveRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Eleve $eleve)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Eleve $eleve)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEleveRequest $request, Eleve $eleve)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Eleve $eleve)
+    {
+        //
+    }
+}
