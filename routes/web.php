331272a0fa62e5f4acbf6cc0e193ae379f956617
molengeek <saoussan.elk@gmail.com>
2025-08-19 12:38:13 +0200

    fin

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/batiment', [BatimentController::class, 'index'])->name('batiment');
 Route::get('/formation', [FormationController::class, 'index'])->name('formation');
 Route::get('/typeformation', [TypeFormationController::class, 'index'])->name('typeformation');
-Route::get('/eleves', [EleveController::class, 'index'])->name('eleves');
-Route::get('/eleves/create', [EleveController::class, 'create'])->name('eleves.create');
-Route::post('/eleves', [EleveController::class, 'store'])->name('eleves.store');
-Route::get('/eleves/{eleves}/edit', [EleveController::class, 'edit'])->name('eleves.edit');
-Route::put('/eleves/{eleves}', [EleveController::class, 'update'])->name('eleves.update');
-Route::delete('/eleves/{eleves}', [EleveController::class, 'destroy'])->name('eleves.destroy');
 
-// Route::resource('eleves', EleveController::class);
+Route::resource('eleves', EleveController::class);
+
+// Route::get('/eleves', [EleveController::class, 'index'])->name('eleves');
+// Route::get('/eleves/create', [EleveController::class, 'create'])->name('eleves.create');
+// Route::post('/eleves', [EleveController::class, 'store'])->name('eleves.store');
+// Route::get('/eleves/{eleves}/edit', [EleveController::class, 'edit'])->name('eleves.edit');
+// Route::put('/eleves/{eleves}', [EleveController::class, 'update'])->name('eleves.update');
+// Route::delete('/eleves/{eleves}', [EleveController::class, 'destroy'])->name('eleves.destroy');
